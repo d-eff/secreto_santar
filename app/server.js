a4cc9d104be7afb6c2be03c5a1846c5e7376446f
David Fernandez <dfernandez06@gmail.com>
2017-02-12 23:13:37 -0500

    Add mongo and move routes out of main

--- app/server.js
+++ app/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const config = require('../config/config.js');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/web/index.html');
+MongoClient.connect(config.mongoURL, function(err, db) {
+  console.log("connected to db");
+
+  db.close();
 });
 
-app.listen(3000, function() {
+app.use(require('./routes.js'));
+
+app.listen(3000, () => {
   console.log(__dirname);
   console.log("listening on 3000");
 });
