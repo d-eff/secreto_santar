cd17a9b9481dc50fc5711683685246e01e425fe3
David Fernandez <dfernandez06@gmail.com>
2017-02-12 22:02:56 -0500

    Right, right, express.

--- app/server.js
+++ app/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/web/index.html');
+});
+
 app.listen(3000, function() {
+  console.log(__dirname);
   console.log("listening on 3000");
 });
