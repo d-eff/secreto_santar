5907ac883fdc6adf0129e1961d4b04464865eb4e
David Fernandez <dfernandez06@gmail.com>
2017-03-20 22:06:43 -0400

    Well, that db connector was short-lived. Added mongoose.

---
+++ app/models/gift.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose'),
+      Schema = mongoose.Schema;
+
+var gift = new Schema({
+    name: String,
+    year: Number,
+    year_id: Schema.Types.ObjectId,
+    from: String,
+    from_id: Schema.Types.ObjectId,
+    to: String,
+    to_id: Schema.Types.ObjectId
+});
+
+module.exports = mongoose.model('Gift', gift, 'Gifts');
--- app/server.js
+++ app/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const config = require('../config/config.js');
-const db = require('./db');
+const bodyParser = require('body-parser');
+// const db = require('./db');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.static('web'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(require('./routes.js'));
 
-db.connect((err) => {
+mongoose.connect(config.mongoURL, (err, db) => {
+        if (err) return cb(err);
+
   if (err) { 
     process.exit(1); 
   } else {
