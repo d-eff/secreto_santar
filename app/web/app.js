595afb8cb2be759e7eacdeecd9aab37ef76e4a5c
David Fernandez <dfernandez06@gmail.com>
2018-04-28 15:56:16 -0400

    what in the actual fuck, remember when making a site consisted of one index.php file and shit was actually fun

--- app/web/app.js
+++ app/web/app.js
@@ -1,7 +1,7 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: "hi"
-  }
-})  
+import Vue from 'vue'
+import App from './app.vue'
 
+new Vue({
+  el: 'body',
+  components: { App }
+})
--- app/web/webpack.config.js
+++ app/web/webpack.config.js
@@ -1,9 +1,27 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, "app.js"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.vue/,
+        loader: 'vue'
+      }
+    ]
+  },
+  vue: {
+    loaders: {
+      js: 'babel'
+    }
   }
 }
